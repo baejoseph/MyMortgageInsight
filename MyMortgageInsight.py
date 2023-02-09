c982efce2e8ce4c5505e332659c29a2402bdddfd
Joseph Bae <baejoseph@gmail.com>
2023-02-09 09:38:21 +0000

    update MyMortgageInsight.py
    got rid of the ":" in some result sentences.

--- MyMortgageInsight.py
+++ MyMortgageInsight.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-
 def monthly_payment(PV,interest,years):
     for payment in range(100,800,1):
         ePV = 1e5
@@ -56,13 +55,9 @@ def early_repayment_value(PV,ER, current_monthly_payment,interest):
         
     return (y1-y2)*12*current_monthly_payment
 
-
-
 st.title("My Mortgage Insight")
 
-
 st.header("Tell us about your current Mortgage:")
-
 
 current_monthly_payment = st.slider('How much are you paying every month?', 300, 6000, 2023, format="£%d")
 years = st.slider('How many years do you have left on your mortgage?', 7, 35, 21, format="%d years")
@@ -73,19 +68,16 @@ Remaining = PV_implicit(current_monthly_payment, current_interest,years)
 st.subheader(f"You are currently paying: £{round(current_monthly_payment):,} at {current_interest}% interest over {years} years.")
 st.subheader(f"Based on what you have told us, you still have £{round(Remaining):,} to pay over {years} years.")
 
-
 st.header("My Future Mortgage")
 
 new_interest = st.slider('What new interest rate are you considering?', 0.1, 9.0, current_interest, format="%f%%")
 
-
 new_monthly_payment = monthly_payment(Remaining, new_interest,years)
 
-st.subheader(f"With a new interest rate of {new_interest}%, your new monthly payment will be: £{round(new_monthly_payment):,}.")
+st.subheader(f"With a new interest rate of {new_interest}%, your new monthly payment will be £{round(new_monthly_payment):,}.")
 
 if new_interest > current_interest:
-    st.subheader(f"This is an increase of: £{round(new_monthly_payment - current_monthly_payment):,}.")
-
+    st.subheader(f"This is an increase of £{round(new_monthly_payment - current_monthly_payment):,}.")
 
 st.header("Value of My One-off Early Repayment")
 
