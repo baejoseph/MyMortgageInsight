2a81f54b8bb5f5d0ef12c0968ee1cc3f1d1d3902
Joseph Bae <baejoseph@gmail.com>
2023-02-12 08:19:13 +0000

    update error
    month year text + normalisation error

--- MyMortgageInsight.py
+++ MyMortgageInsight.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 
 def monthly_payment(PV,interest,years):
-    for payment in range(100,800,1):
+    for payment in range(60,800,1):
         ePV = 1e5
         for month in range(years*12):
             ePV *= (1+interest/1200)
@@ -55,6 +55,18 @@ def early_repayment_value(PV,ER, current_monthly_payment,interest):
         
     return (y1-y2)*12*current_monthly_payment
 
+def years_months_text(yr):
+    years_, months_ = divmod((yr)*12, 12)
+    years_text, months_text = "",""
+    if years_ > 0: 
+        years_text = f"{round(years_)} year"
+        if years_ > 1: years_text += "s" 
+    if months_ > 0: 
+        months_text = f"{round(months_)} month"
+        if months_ > 1: months_text += "s"
+    if (months_ > 0) and (years_ > 0): years_text += " and "
+    return years_text + months_text
+
 st.title("My Mortgage Insight")
 
 st.header("Tell us about your current Mortgage:")
@@ -67,7 +79,7 @@ Remaining = PV_implicit(current_monthly_payment, current_interest,years)
 repayment = 0
 
 st.subheader(f"You are currently paying £{round(current_monthly_payment):,} at {current_interest}% interest over {years} years.")
-st.subheader(f"Based on what you have told us, you still have £{round(Remaining):,} to pay over {years} years.")
+st.subheader(f"Based on what you have said, you still have £{round(Remaining):,} to pay over {years} years.")
 
 st.header("My Future Mortgage")
 
@@ -75,12 +87,12 @@ new_interest = st.slider('What new interest rate are you considering?', 0.1, 9.0
 
 extension = st.slider('Are you thinking of extending the term at all?', 0, 15, 0, format="%d years")
 extension_text = ""
-if extension > 0: extension_text = f" and extending the term by {extension} years,"
+if extension > 0: extension_text = f" and extending the term by {years_months_text(extension)},"
 
 new_term = years+extension   
 new_monthly_payment = monthly_payment(Remaining, new_interest,years+extension)
 
-st.subheader(f"With a new interest rate of {new_interest}%,{extension_text} your new monthly payment will be £{round(new_monthly_payment):,} over {new_term} years.")
+st.subheader(f"With a new interest rate of {new_interest}%,{extension_text} your new monthly payment will be £{round(new_monthly_payment):,} over {years_months_text(new_term)}.")
 
 if (new_interest > current_interest) or (extension > 0):
     if new_monthly_payment > current_monthly_payment: 
@@ -97,21 +109,12 @@ repayment = st.slider('How much extra are you willing to pay today?', 0,round(Re
 # value of repayment if reducing terms
 value = early_repayment_value(Remaining,repayment, new_monthly_payment,new_interest)
 new_term = number_years(Remaining-repayment,new_monthly_payment,new_interest)
-years_delta, months_delta = divmod((years+extension-new_term)*12, 12)
-years_delta_text, months_delta_text = "",""
-if years_delta > 0: 
-    years_delta_text = f"{round(years_delta)} year"
-    if years_delta > 1: years_delta_text += "s" 
-if months_delta > 0: 
-    months_delta_text = f"{round(months_delta)} month"
-    if months_delta > 1: months_delta_text += "s"
-if (months_delta > 0) and (years_delta > 0): years_delta_text += " and "
 
 # value of repayment if reducing monthly payment
 updated_monthly_payment = monthly_payment(Remaining - repayment, new_interest,years+extension)
 value2 = (new_monthly_payment- updated_monthly_payment)*12*(years+extension)
 
 if repayment > 0:
-    st.subheader(f'If you repay an extra £{round(repayment):,} now, you can either shorten the term by {years_delta_text}{months_delta_text} and pay £{round(value):,} less over {new_term} years at {new_interest}% interest; or reduce monthly payment by £{round(new_monthly_payment- updated_monthly_payment)} and pay £{round(value2):,} less over {years+extension} years at {new_interest}% interest.')
+    st.subheader(f'If you repay an extra £{round(repayment):,} now, you can either shorten the term by {years_months_text(years+extension-new_term)} and pay £{round(value):,} less over {years_months_text(new_term)} at {new_interest}% interest; or reduce monthly payment by £{round(new_monthly_payment- updated_monthly_payment)} and pay £{round(value2):,} less over {years_months_text(years+extension)} at {new_interest}% interest.')
     
 st.write("Copyright © 2023 Joseph Bae")
