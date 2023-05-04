88f76f7e45c59c83eff7629842f838d6f513df99
Joseph Bae <baejoseph@gmail.com>
2023-05-04 09:20:26 +0100

    just copy and paste
    the buy me button code

--- MyMortgageInsight.py
+++ MyMortgageInsight.py
@@ -2,8 +2,50 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-from streamlit_extras.buy_me_a_coffee import button
 from streamlit_extras.add_vertical_space import add_vertical_space
+
+def button(
+    username: str,
+    floating: bool = True,
+    text: str = "Buy me a coffee",
+    emoji: str = "",
+    bg_color: str = "#FFDD00",
+    font: Font = "Cookie",
+    font_color: str = "#000000",
+    coffee_color: str = "#000000",
+    width: int = 220,
+):
+    button = f"""
+        <script type="text/javascript"
+            src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js"
+            data-name="bmc-button"
+            data-slug="{username}"
+            data-color="{bg_color}"
+            data-emoji="{emoji}"
+            data-font="{font}"
+            data-text="{text}"
+            data-outline-color="#000000"
+            data-font-color="{font_color}"
+            data-coffee-color="{coffee_color}" >
+        </script>
+    """
+
+    html(button, height=70, width=width)
+
+    if floating:
+        st.markdown(
+            f"""
+            <style>
+                iframe[width="{width}"] {{
+                    position: fixed;
+                    bottom: 60px;
+                    right: 40px;
+                }}
+            </style>
+            """,
+            unsafe_allow_html=True,
+        )
+
 
 def monthly_payment(PV,interest,years):
     for payment in range(60,800,1):
